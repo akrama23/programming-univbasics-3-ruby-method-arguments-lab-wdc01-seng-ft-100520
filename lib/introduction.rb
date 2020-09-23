ac2059e9ddeb7910b12767c2df6c6ffdbb3411ed
akam alam <arundelfootball23@gmail.com>
2020-09-23 02:26:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,15 +5,15 @@
 
 
 def introduction(name)
-  puts "Hi, my name is #{name}."
+  return "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  return  "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-def introduction_with_language_optional(name, language = "Python")
-   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name, language = "Ruby")
+   return "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction("Josh")
